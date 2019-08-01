4a230ad78515120b986dcca457f7eaa19f506564
DJ Adams <dj.adams@sap.com>
2019-08-01 08:24:42 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const cds = require("@sap/cds")
+
+const { PORT=3000 } = process.env
+const app = express()
+
+cds.serve("all").in(app)
+
+app.listen (PORT, ()=> console.info(`server listening on port ${PORT}`
+
+// Seed with sample data
+cds.deploy('srv').to('sqlite::memory:',{primary:true}) .then (async db => {
+
+	const { A_BusinessPartnerAddress: Addresses } = db.entities('API_BUSINESS_PARTNER')
+	console.log('Adding sample data...')
+
+	const addresses = db.run (INSERT.into (Addresses+'') .entries (
+		{
+			BusinessPartner: '1003764',
+			AddressID: '28238',
+			CityName: 'Walldorf',
+			StreetName: 'Dietmar-Hopp-Allee'
+		},
+		{
+			BusinessPartner: '1003765',
+			AddressID: '28241',
+			CityName: 'Palo Alto',
+			StreetName: 'Hillview Avenue'
+		},
+		{
+			BusinessPartner: '1003766',
+			AddressID: '28244',
+			CityName: 'Hallbergmoos',
+			StreetName: 'Zeppelinstraße'
+		},
+		{
+			BusinessPartner: '1003767',
+			AddressID: '28247',
+			CityName: 'Potsdam',
+			StreetName: 'Konrad-Zuse-Ring'
+		}
+	))
+
+  await Promise.all ([addresses])
+
+}) .catch (console.error)
---
+++ srv/cat-service.cds
@@ -0,0 +1,10 @@
+using API_BUSINESS_PARTNER as BUPA_API from './external/csn/API_BUSINESS_PARTNER.json';
+
+service API_BUSINESS_PARTNER @(path: '/sap/opu/odata/sap/API_BUSINESS_PARTNER') {
+	@cds.persistence.skip:false
+	@cds.persistence.table
+	entity A_BusinessPartner as projection on BUPA_API.A_BusinessPartnerType;
+	@cds.persistence.skip:false
+	@cds.persistence.table
+	entity A_BusinessPartnerAddress as projection on BUPA_API.A_BusinessPartnerAddressType;
+}
